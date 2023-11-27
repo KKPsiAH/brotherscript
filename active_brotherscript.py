769cd79c5423d4b694861c50ae7e14a8b71e175a
Ryan Lu <ryan@ryanlu.dev>
2023-11-27 17:09:24 -0500

    Update logic for row of less than 4 brothers

--- active_brotherscript.py
+++ active_brotherscript.py
@@ -17,10 +17,27 @@ def create_brother_from_csv_row(row):
                    major= row[5],
                    minor= row[6])
     return b 
+    
+def create_empty_card():
+    e = br.brother(name= "NULL",
+                   picture= "KappaKappaPsiForCards.png",
+                   pronouns= "NULL",
+                   initiation_class= "NULL",
+                   hometown= "NULL",
+                   instrument= "NULL",
+                   major= "NULL",
+                   minor= "NULL")
+    return e
 
 def build_brother_dict(csv_file: str):
+    num_lines = 0
+    with open(csv_file, newline='', encoding="utf-8-sig") as csvfile:
+        csvreader1 = csv.reader(csvfile)
+        num_lines = len(list(csvreader1))
     with open(csv_file, newline='', encoding="utf-8-sig") as csvfile:
         csvreader = csv.reader(csvfile)
+        empty_cards = 4 - (num_lines % 4)
+        countlines = 0
         index = 0
         current_column = 1
         brother_row_col_dict = {}
@@ -41,12 +58,25 @@ def build_brother_dict(csv_file: str):
                     index += 1
                     current_column = 1
                     brother_row_col_dict = {}
+            countlines += 1
+            if countlines == num_lines and empty_cards != 4:
+                for i in range(empty_cards):
+                    brother_row_col_dict[current_column] = create_empty_card()
+                    current_column +=1
+                brother_dict[index] = brother_row_col_dict
+                
 
-def brother_row_builder(p: page.page, brother_row_col_dict: dict):
+def brother_row_builderL(p: page.page, brother_row_col_dict: dict):
     column = 1
+    while(column < 3):
+        p.add_brothers(img_prefix = img_prefix,brother = brother_row_col_dict[column])
+        column+= 1        
+
+def brother_row_builderR(p: page.page, brother_row_col_dict: dict):
+    column = 3
     while(column < 5):
         p.add_brothers(img_prefix = img_prefix,brother = brother_row_col_dict[column])
-        column+= 1            
+        column+= 1    
 
 build_brother_dict('active.csv')
 
@@ -55,7 +85,9 @@ p = page.page(a = Airium())
 for index in brother_dict.keys():
     with p.a.div(id = "brother-row"):
         with p.a.div(id = "brother-row-group"):
-            brother_row_builder(p, brother_dict[index])
+            brother_row_builderL(p, brother_dict[index])
+        with p.a.div(id = "brother-row-group"):
+            brother_row_builderR(p, brother_dict[index])
     p.a.break_source_line()
 
 html = str(p.a).encode('utf-8')
--- brothers_page.py
+++ brothers_page.py
@@ -6,23 +6,42 @@ class page:
         self.a: Airium = a
         
     def add_brothers(self, img_prefix, brother:br.brother):
-        with self.a.div(id = "brother-card"):
-            with self.a.div(klass = "card"):
-                self.a.img(klass = "card-img-top", src=(img_prefix+brother.picture), alt=brother.name, onerror="this.onerror=null;this.src='img\\\logos\\\KappaKappaPsiForCards.png'")
-                with(self.a.div(klass = "card-body")):
-                    with(self.a.h3(klass = "card-text")):
-                        self.a(brother.name)
-                    with(self.a.h6(klass = "card-text")):
-                        self.a(f"<i>{brother.pronouns}</i>")
-                    with(self.a.h5(klass = "card-text")):
-                        self.a(brother.initiation_class)
-                    with(self.a.h5(klass = "card-text")):
-                        self.a(brother.instrument)
-                    with(self.a.h5(klass = "card-text")):
-                        self.a(f"Majors(s): {brother.major}")
-                    if(brother.minor != "N/A"):
+        if brother.name == "NULL":
+            with self.a.div(id = "hidden-card"):
+                with self.a.div(klass = "card"):
+                    self.a.img(klass = "card-img-top", src=(img_prefix+brother.picture), alt=brother.name, onerror="this.onerror=null;this.src='img\\\logos\\\KappaKappaPsiForCards.png'")
+                    with(self.a.div(klass = "card-body")):
+                        with(self.a.h3(klass = "card-text")):
+                            self.a(brother.name)
+                        with(self.a.h6(klass = "card-text")):
+                            self.a(f"<i>{brother.pronouns}</i>")
                         with(self.a.h5(klass = "card-text")):
-                           self.a(f"Minors(s): {brother.minor}")
+                            self.a(brother.initiation_class)
+                        with(self.a.h5(klass = "card-text")):
+                            self.a(brother.instrument)
+                        with(self.a.h5(klass = "card-text")):
+                            self.a(f"Majors(s): {brother.major}")
+                        if(brother.minor != "N/A"):
+                            with(self.a.h5(klass = "card-text")):
+                               self.a(f"Minors(s): {brother.minor}")
+        else:
+            with self.a.div(id = "brother-card"):
+                with self.a.div(klass = "card"):
+                    self.a.img(klass = "card-img-top", src=(img_prefix+brother.picture), alt=brother.name, onerror="this.onerror=null;this.src='img\\\logos\\\KappaKappaPsiForCards.png'")
+                    with(self.a.div(klass = "card-body")):
+                        with(self.a.h3(klass = "card-text")):
+                            self.a(brother.name)
+                        with(self.a.h6(klass = "card-text")):
+                            self.a(f"<i>{brother.pronouns}</i>")
+                        with(self.a.h5(klass = "card-text")):
+                            self.a(brother.initiation_class)
+                        with(self.a.h5(klass = "card-text")):
+                            self.a(brother.instrument)
+                        with(self.a.h5(klass = "card-text")):
+                            self.a(f"Majors(s): {brother.major}")
+                        if(brother.minor != "N/A"):
+                            with(self.a.h5(klass = "card-text")):
+                               self.a(f"Minors(s): {brother.minor}")
     
     # TODO: Flesh out officer card functions
     # def add_brother_officers(self, img_prefix, officer:br.officer):
